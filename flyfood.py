cbcc95d939861e3fd38ce809120433c7ab77e11b
Gui-pdf <160535069+Gui-pdf@users.noreply.github.com>
2024-11-25 19:53:44 -0300

    Update flyfood.py
    Import removido
    Distância euclideana
    Indices da função de mapeamento corrigidos:
    (y, x), começando pelo zero
    
    novo print de teste adicionado: a matriz mapa
    remover prints desnecessários na versão final do código

--- flyfood.py
+++ flyfood.py
@@ -1,42 +1,39 @@
-import itertools
-
 arquivo_entrada = 'mapa.txt'
 
 def abrir_mapa(mapa_arquivo:str): # retorna um dicionário com as coordenadas de cada ponto de interesse no mapa.
-    # essas coordenadas são contadas começando do 1.
+    # essas coordenadas são contadas começando do 0.
     matriz_mapa = {}
     y, x = 0, 0 # armazena as coordenadas que estão sendo lidas em cada loop.
     with open(mapa_arquivo, 'r') as arquivo_de_entrada:
         mapa = arquivo_de_entrada.read()
         len_y, len_x = 0, 0
         for i in mapa:
-        
-            if i == ' ': # ignora os espaços
+
+            if i == ' ' or i == '\n': # ignora os espaços e quebras de linha
                 continue
 
-            elif len_x == 0: # para verificar os dois primeiros números
+            if len_x == 0: # para verificar os dois primeiros números
                 if len_y == 0: # coloca o primeiro número como tamanho Y
                     len_y = int(i)
                 else: # coloca o segundo número como tamanho X
                     len_x = int(i)
+                continue
 
-            elif i == '\n':
-                y += 1
+            if x == len_x:
                 x = 0
-            
-            else:
-                
-                x += 1
-                if i != '0':
-                    matriz_mapa[i] = (x, y)
-        
+                y += 1
+
+            if i != '0':
+                matriz_mapa[i] = (y,x)
+            x += 1
+    print(matriz_mapa) # remover essa linha no código final
     return matriz_mapa
 
 
 def get_distancia(a,b,matriz): # encontra a distancia entre 2 casas
-    distancia_x = abs(matriz[a][0] - matriz[b][0])
-    distancia_y = abs(matriz[a][1] - matriz[b][1])
-    return distancia_x + distancia_y
+    distancia_y = abs(matriz[a][0] - matriz[b][0])
+    distancia_x = abs(matriz[a][1] - matriz[b][1])
+    return (distancia_x ** 2 + distancia_y ** 2) ** 0.5 # Distância euclideana
 
 
 def salvar(permutacoes, permutacao):
@@ -55,7 +52,7 @@ def permutar(permutacoes, lista, passo = 0):
 
         lista[passo], lista[index] = lista[index], lista[passo] # Restaura estado da linha para evitar repetição 
     return permutacoes
-        
+
 
 def forca_bruta(matriz):
     casas = list(matriz)
@@ -78,9 +75,9 @@ def forca_bruta(matriz):
             if tamanho < tamanho_menor:
                 caminho_menor = caminho
                 tamanho_menor = tamanho
-    
+
     return caminho_menor, tamanho_menor # no código final, retorne apenas o caminho.
-        
+
 
 
 mapa = abrir_mapa(arquivo_entrada)
@@ -88,4 +85,4 @@ caminho, tamanho = forca_bruta(mapa)
 for casa in caminho:
     print(casa, end=' ')
 print()
-print('distância percorrida:', tamanho)
+print('distância percorrida: %.2fdm (dronômetros)' % tamanho)
