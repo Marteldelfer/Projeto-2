5eb5b8956a5ac275c63777e8ff2be9e891e30ddf
Gui-pdf <160535069+Gui-pdf@users.noreply.github.com>
2025-01-08 17:53:20 -0300

    Update flyfood.py
    mudança de nome de variáveis para consistência e removida a função salvar

--- flyfood.py
+++ flyfood.py
@@ -1,8 +1,8 @@
 arquivo_entrada = 'mapa.txt'
 
-def abrir_mapa(mapa_arquivo:str): # retorna um dicionário com as coordenadas de cada ponto de interesse no mapa.
-    # essas coordenadas são contadas começando do 0.
-    matriz_mapa = {}
+def abrir_mapa(mapa_arquivo:str): # retorna um dicionário com as coordenadas de cada ponto de interesse no mapa
+    # essas coordenadas são contadas começando do 0, da esquerda para a direita no x e de cima para baixo no y
+    matriz_entrada = {}
     y, x = 0, 0 # armazena as coordenadas que estão sendo lidas em cada loop.
     with open(mapa_arquivo, 'r') as arquivo_de_entrada:
         mapa = arquivo_de_entrada.read()
@@ -24,30 +24,27 @@ def abrir_mapa(mapa_arquivo:str): # retorna um dicionário com as coordenadas de
                 y += 1
 
             if i != '0':
-                matriz_mapa[i] = (y,x)
+                matriz_entrada[i] = (y,x)
             x += 1
-    return matriz_mapa
+
+    return matriz_entrada
 
 
-def get_distancia(a,b,matriz): # encontra a distancia entre 2 casas
+def distancia(a,b,matriz): # encontra a distancia entre 2 casas
     distancia_y = abs(matriz[a][0] - matriz[b][0])
     distancia_x = abs(matriz[a][1] - matriz[b][1])
     return (distancia_x ** 2 + distancia_y ** 2) ** 0.5 # Distância euclideana
 
 
-def salvar(permutacoes, permutacao):
-    permutacoes.append(permutacao)
-    return permutacoes
-
-def permutar(permutacoes, lista, passo = 0):
+def permutar(lista, permutacoes = [], passo = 0):
 
     if passo == len(lista): # Caso base
-        salvar(permutacoes, lista[:]) # Salva uma cópia da lista se tiver terminado as permutações
+        permutacoes.append(lista[:]) # Salva uma cópia da lista se tiver terminado as permutações
 
     for index in range(passo, len(lista)): 
 
         lista[passo], lista[index] = lista[index], lista[passo]
-        permutar(permutacoes, lista, passo + 1)
+        permutar(lista, permutacoes, passo + 1)
 
         lista[passo], lista[index] = lista[index], lista[passo] # Restaura estado da linha para evitar repetição 
     return permutacoes
@@ -57,20 +54,20 @@ def forca_bruta(matriz):
     casas = list(matriz)
     casas.remove('R')
 
-    permutacoes = permutar([], casas)
+    permutacoes = permutar(casas)
 
     caminho_menor, tamanho_menor = (), float('inf')
     for caminho in permutacoes:
         tamanho = 0
         casa_anterior = 'R' #começa pelo R
         for casa in caminho:
-            tamanho += get_distancia(casa, casa_anterior, matriz)
+            tamanho += distancia(casa, casa_anterior, matriz)
             casa_anterior = casa
-            if tamanho > tamanho_menor: # para o loop caso o tamanho seja maior que o esperado.
+            if tamanho > tamanho_menor: # para o loop caso o tamanho seja maior que o esperado
                 break
 
-        else: # retorna ao inicio
-            tamanho += get_distancia(casa_anterior, 'R', matriz)
+        else: # retorna ao inicio e confere se é o menor
+            tamanho += distancia(casa_anterior, 'R', matriz)
             if tamanho < tamanho_menor:
                 caminho_menor = caminho
                 tamanho_menor = tamanho
@@ -79,8 +76,8 @@ def forca_bruta(matriz):
 
 
 
-mapa = abrir_mapa(arquivo_entrada)
-caminho, tamanho = forca_bruta(mapa)
+matriz_de_entrada = abrir_mapa(arquivo_entrada)
+caminho, tamanho = forca_bruta(matriz_de_entrada)
 for casa in caminho:
     print(casa, end=' ')
 print()
