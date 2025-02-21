d626e0c9f4dc365fcbc4e81343a332184d11026d
Murillo Ferraz <marteldelfer@gmail.com>
2025-02-21 17:53:34 -0300

    Pequenas alterações

--- flyfood_genetico.py
+++ flyfood_genetico.py
@@ -3,26 +3,17 @@ from typing import List, Tuple, Dict, Iterable
 
 Grafo = Dict[int, Dict[int, float]]
 
-def soma(iteravel : Iterable[float]) -> float:
-    res = 0
-    for i in iteravel:
-        res += i
-    return res
-
-def distancia(x1 : float, y1 : float, x2 : float, y2 : float) -> float:
-    return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
-
-def abrir_arquivo() -> List[Tuple[float, float]]:
-    with open("berlin52.csv") as f:
+def abrir_arquivo(nome_arquivo : str = "berlin52.csv") -> List[Tuple[float, float]]:
+    with open(nome_arquivo) as f:
         b52 = csv.reader(f)
         cordenadas = []
         for linha in b52:
             cordenadas.append((float(linha[0]), float(linha[1])))
     return cordenadas
 
-def gerar_grafo() -> Grafo:
+def gerar_grafo(nome_arquivo : str = "berlin52.csv") -> Grafo:
     grafo = dict()
-    cordenadas = abrir_arquivo()
+    cordenadas = abrir_arquivo(nome_arquivo)
 
     for i, ponto_a in enumerate(cordenadas):
         distancias = dict()
@@ -33,27 +24,30 @@ def gerar_grafo() -> Grafo:
         grafo[i] = distancias
     return grafo
 
+def distancia(x1 : float, y1 : float, x2 : float, y2 : float) -> float:
+    return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+
 def distancia_caminho(grafo : Grafo, caminho : List[int]) -> float:
     res = grafo[0][caminho[0]]
     for i in range(1, len(caminho) - 1):
         res += grafo[i][i + 1]
     return res + grafo[caminho[-1]][0]
 
-def gerar_caminho_aleatorio() -> List[int]:
-    res = list(range(1,52))
+def gerar_caminho_aleatorio(grafo : Grafo) -> List[int]:
+    res = list(range(1,len(grafo)))
     random.shuffle(res)
     return res
 
 def aptidao(grafo : Grafo, caminho : List[int]) -> float:
     return 1 / distancia_caminho(grafo, caminho)
 
-def selecionar_pais(grafo : Grafo, candidatos : List[List[int]], teste = "aptidao") -> List[List[int]]:
+def selecionar_pais(grafo : Grafo, candidatos : List[List[int]], teste = "torneio") -> List[List[int]]:
     pais = []
 
     if teste == "aptidao":
         for _ in range(len(candidatos) // 2):
             aptidoes = [aptidao(grafo, candidato) for candidato in candidatos]
-            soma_aptidoes = soma(aptidoes)
+            soma_aptidoes = sum(aptidoes)
             aptidoes = [i / soma_aptidoes for i in aptidoes]
             chance = random.random()
 
@@ -88,9 +82,10 @@ def crossover(a : List[int], b : List[int]) -> Tuple[List[int], List[int]]:
     return res_a, res_b
 
 def mutacao(caminho : List[int], p : float = 0.1) -> None:
-    if p >= random.random():
-        i_gene_a, i_gene_b = random.randrange(len(caminho)), random.randrange(len(caminho))
-        caminho[i_gene_a], caminho[i_gene_b] = caminho[i_gene_b], caminho[i_gene_a]
+    for i in range(len(caminho)):
+        if p >= random.random():
+            i_gene = random.randrange(len(caminho))
+            caminho[i_gene], caminho[i] = caminho[i], caminho[i_gene]
 
 def gerar_filhos(pais : List[List[int]], p_mutacao : float = 0.1, p_filho : float = 0.95) -> List[List[int]]:
     filhos = []
@@ -106,31 +101,33 @@ def gerar_filhos(pais : List[List[int]], p_mutacao : float = 0.1, p_filho : floa
         mutacao(filho, p_mutacao)
     return filhos
 
-def genetico():
-    grafo = gerar_grafo()
-    pais = [gerar_caminho_aleatorio() for _ in range(40)]
-    for _ in range(500):
+def genetico(
+        nome_arquivo : str = "berlin52.csv",
+        n_populacao : int = 40,
+        n_geracoes : int = 1000,
+        teste = "torneio"
+):
+    grafo = gerar_grafo(nome_arquivo)
+    pais = [gerar_caminho_aleatorio(grafo) for _ in range(n_populacao)]
+
+    for _ in range(n_geracoes):
         filhos = gerar_filhos(pais)
-        pais = selecionar_pais(grafo, filhos, teste="torneio")
-    return pais
+        pais = selecionar_pais(grafo, filhos, teste)
+    
+    return min(pais, key=lambda x: distancia_caminho(grafo, x))
 
 def plotar_caminho(caminho):
     data = abrir_arquivo()
     for x,y in data:
         plt.scatter(x,y, c="black", s=16)
-    caminho = gerar_caminho_aleatorio()
     for a, b in zip(caminho, caminho[1:]):
         plt.plot([data[a][0], data[b][0]], [data[a][1], data[b][1]], c = "black")
     plt.show()
 
 
-print(distancia_caminho(gerar_grafo(), min(genetico(), key=lambda x: distancia_caminho(gerar_grafo(), x))))
-print(distancia_caminho(gerar_grafo(), min(genetico(), key=lambda x: distancia_caminho(gerar_grafo(), x))))
-print(distancia_caminho(gerar_grafo(), min(genetico(), key=lambda x: distancia_caminho(gerar_grafo(), x))))
-print(distancia_caminho(gerar_grafo(), min(genetico(), key=lambda x: distancia_caminho(gerar_grafo(), x))))
+if __name__ == "__main__":
+    caminho = genetico()
+    grafo = gerar_grafo()
 
-plotar_caminho(min(genetico(), key=lambda x: distancia_caminho(gerar_grafo(), x)))
-plotar_caminho(min(genetico(), key=lambda x: distancia_caminho(gerar_grafo(), x)))
-plotar_caminho(min(genetico(), key=lambda x: distancia_caminho(gerar_grafo(), x)))
-plotar_caminho(min(genetico(), key=lambda x: distancia_caminho(gerar_grafo(), x)))
-
+    print(distancia_caminho(grafo, caminho))
+    plotar_caminho(caminho)
