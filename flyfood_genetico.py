008a9499a5d1c33fb713c098d649933de6740bfb
Murillo Ferraz <marteldelfer@gmail.com>
2025-02-22 00:41:34 -0300

    ajeitar codigo

--- flyfood_genetico.py
+++ flyfood_genetico.py
@@ -1,4 +1,4 @@
-import csv, math, random, matplotlib.pyplot as plt, numpy as np
+import csv, math, random, tqdm, matplotlib.pyplot as plt, numpy as np
 from typing import List, Tuple, Dict, Iterable
 
 Grafo = Dict[int, Dict[int, float]]
@@ -11,12 +11,9 @@ def abrir_arquivo(nome_arquivo : str = "berlin52.csv") -> List[Tuple[float, floa
             cordenadas.append((float(linha[0]), float(linha[1])))
     return cordenadas
 
-def gerar_grafo(nome_arquivo : str = "berlin52.csv", aleatorio=False,tam=52) -> Grafo:
+def gerar_grafo(nome_arquivo : str = "berlin52.csv") -> Grafo:
     grafo = dict()
-    if not aleatorio:
-        cordenadas = abrir_arquivo(nome_arquivo)
-    else:
-        cordenadas = [(random.randint(1,1000), random.randint(1,1000)) for _ in range(tam)]
+    cordenadas = abrir_arquivo(nome_arquivo)
 
     for i, ponto_a in enumerate(cordenadas):
         distancias = dict()
@@ -31,64 +28,65 @@ def distancia(x1 : float, y1 : float, x2 : float, y2 : float) -> float:
     return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
 
 def distancia_caminho(grafo : Grafo, caminho : List[int]) -> float:
-    res = grafo[0][caminho[0]]
-    for i in range(1, len(caminho) - 1):
-        res += grafo[i][i + 1]
-    return res + grafo[caminho[-1]][0]
+    res = 0
+    for a, b in zip(caminho, caminho[1:]):
+        res += grafo[a][b]
+    res += grafo[caminho[-1]][caminho[0]]
+    return res
+
+def melhor_distancia(grafo : Grafo, caminhos : List[List[int]]) -> Tuple[float, List[int]]:
+    m_distancia = float('inf')
+    m_caminho = None
+
+    for caminho in caminhos:
+        nova_distancia = distancia_caminho(grafo, caminho)
+        if nova_distancia < m_distancia:
+            m_distancia = nova_distancia
+            m_caminho = caminho
+    return m_distancia, m_caminho
 
 def gerar_caminho_aleatorio(grafo : Grafo) -> List[int]:
-    res = list(range(1,len(grafo)))
+    res = list(range(0,len(grafo)))
     random.shuffle(res)
     return res
 
-def aptidao(grafo : Grafo, caminho : List[int]) -> float:
-    return 1 / distancia_caminho(grafo, caminho)
-
-def selecionar_pais(grafo : Grafo, candidatos : List[List[int]], teste = "torneio") -> List[List[int]]:
+def selecionar_pais(grafo : Grafo, candidatos : List[List[int]]) -> List[List[int]]:
     pais = []
+    for _ in range(len(candidatos) // 2):
+        candidatoA = candidatos.pop(random.randrange(0, len(candidatos)))
+        candidatoB = candidatos.pop(random.randrange(0, len(candidatos)))
+        pais.append(candidatoA if distancia_caminho(grafo, candidatoA) < distancia_caminho(grafo, candidatoB) else candidatoB)
+    return pais
 
-    if teste == "aptidao":
-        for _ in range(len(candidatos) // 2):
-            aptidoes = [aptidao(grafo, candidato) for candidato in candidatos]
-            soma_aptidoes = sum(aptidoes)
-            aptidoes = [i / soma_aptidoes for i in aptidoes]
-            chance = random.random()
+def trocar(filho : List[int], indicie : int, gene : int) -> None:
+    for i, g in enumerate(filho):
+        if gene == g:
+            filho[indicie], filho[i] = filho[i], filho[indicie]
+            break 
 
-            for index, i in enumerate(aptidoes):
-                if i > chance:
-                    pais.append(candidatos.pop(index))
-                    break
-                chance -= i
-        return pais
+def pmx(caminho_a : List[int], caminho_b : List[int]) -> Tuple[List[int], List[int]]:
+    separador = random.randrange(0, len(caminho_a))
+    filho_a = caminho_a[:]
+    filho_b = caminho_b[:]
+    filho_c = caminho_a[::-1]
+    filho_d = caminho_b[::-1]
 
-    elif teste == "torneio":
-        pais = []
-        for _ in range(len(candidatos) // 2):
-            candidatoA = candidatos.pop(random.randrange(0, len(candidatos)))
-            candidatoB = candidatos.pop(random.randrange(0, len(candidatos)))
-            pais.append(candidatoA if distancia_caminho(grafo, candidatoA) < distancia_caminho(grafo, candidatoB) else candidatoB)
-        return pais
-
-def pmx(caminho_a : List[int], caminho_b : List[int], separador : int) -> Tuple[List[int], List[int]]:
-
-    for indicie_a, gene_a in enumerate(caminho_a[:separador]):
-        for indicie_b, gene_b in enumerate(caminho_b):
-            if gene_a == gene_b:
-                caminho_b[indicie_a], caminho_b[indicie_b] = caminho_b[indicie_b], caminho_b[indicie_a]
-    return caminho_a, caminho_b 
-
-def crossover(pai_a : List[int], pai_b : List[int]) -> Tuple[List[int], List[int]]:
-    separador = random.randrange(0, len(pai_a))
-    a, b = pmx(pai_a, pai_b, separador)
-    filho_a = a[:separador] + b[separador:]
-    filho_b = b[:separador] + a[separador:]
-    return filho_a, filho_b
+    for i, gene in enumerate(caminho_a[:separador]):
+        trocar(filho_b, i, gene)    
+    for i, gene in enumerate(caminho_b[:separador]):
+        trocar(filho_a, i, gene)
+    for i, gene in enumerate(caminho_a[::-1][:separador]):
+        trocar(filho_d, i, gene)
+    for i, gene in enumerate(caminho_b[::-1][:separador]):
+        trocar(filho_c, i, gene)
+    
+    return filho_a, filho_b, filho_c, filho_d
+        
 
 def mutacao(caminho : List[int], p : float = 0.1) -> None:
-    for i in range(len(caminho)):
-        if p >= random.random():
-            i_gene = random.randrange(len(caminho))
-            caminho[i_gene], caminho[i] = caminho[i], caminho[i_gene]
+    if p > random.random():
+        i_a, i_b = (random.randrange(0, len(caminho)) for _ in range(2))
+        caminho[i_a], caminho[i_b] = caminho[i_b], caminho[i_a]
 
 def gerar_filhos(pais : List[List[int]], p_mutacao : float = 0.1, p_filho : float = 0.95) -> List[List[int]]:
     filhos = []
@@ -96,10 +94,10 @@ def gerar_filhos(pais : List[List[int]], p_mutacao : float = 0.1, p_filho : floa
         pai_a = pais.pop(random.randrange(0, len(pais)))
         pai_b = pais.pop(random.randrange(0, len(pais)))
         if p_filho >= random.random():
-            filhos.extend(crossover(pai_a, pai_b))
-            filhos.extend(crossover(pai_b, pai_a))
+            filhos.extend(pmx(pai_a, pai_b))
         else:
             filhos.extend((pai_a,pai_b,pai_a,pai_b))
+            
     for filho in filhos:
         mutacao(filho, p_mutacao)
     return filhos
@@ -110,15 +108,14 @@ def genetico(
         n_geracoes : int = 1000,
         p_mutacao : float = 0.05,
         p_cruzamento : float = 0.90,
-        teste = "torneio",
-        aleatorio : bool = False
 ):
-    grafo = gerar_grafo(nome_arquivo, aleatorio=aleatorio)
+    grafo = gerar_grafo(nome_arquivo)
     pais = [gerar_caminho_aleatorio(grafo) for _ in range(n_populacao)]
 
-    for _ in range(n_geracoes):
+    for _ in (t:= tqdm.trange(n_geracoes)):
         filhos = gerar_filhos(pais, p_mutacao=p_mutacao, p_filho=p_cruzamento)
-        pais = selecionar_pais(grafo, filhos, teste)
+        pais = selecionar_pais(grafo, filhos)
+        t.set_description(str(distancia_caminho(grafo, pais[0])))
     
     menor_caminho = min(pais, key=lambda x: distancia_caminho(grafo, x))
     return distancia_caminho(grafo, menor_caminho), menor_caminho
@@ -133,5 +130,6 @@ def plotar_caminho(caminho):
 
 
 if __name__ == "__main__":
-    d, caminho = genetico()
-    print(f"{d}\n{caminho}")
+    d, caminho = genetico(n_populacao=40, n_geracoes=5000, p_mutacao=0.05)
+    print(f"{d}  {caminho}")
+    plotar_caminho(caminho)
